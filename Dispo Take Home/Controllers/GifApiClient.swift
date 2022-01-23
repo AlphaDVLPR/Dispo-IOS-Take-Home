12fcd50e903be56651ce62e8302f1a01c32da46e
Jesse Rae <51697381+AlphaDVLPR@users.noreply.github.com>
2022-01-23 15:17:30 -0700

    searchTrending in GifAPIClient controller

--- Dispo Take Home/Controllers/GifApiClient.swift
+++ Dispo Take Home/Controllers/GifApiClient.swift
@@ -1,5 +1,54 @@
 import UIKit
 
+/**
+ All URL's and queries were tested using Postman to save time on my end
+ */
+
 struct GifAPIClient {
   // TODO: Implement
+    static let shared = GifAPIClient()
+    
+    static func searchTrendingGifs(completion : @escaping ([GifObject]) -> Void) {
+        
+        // Example: https://api.giphy.com/v1/gifs/trending?api_key=1XyHwakiUAAS2h7kiIU3lQrIQcHk71a6&limit=25&rating=g
+        
+        //URL
+        var urlComponent = URLComponents()
+        
+        urlComponent.scheme = "https"
+        urlComponent.host = "www.api.giphy.com"
+        urlComponent.path = "/v1/gifs/trending"
+        print(urlComponent)
+        
+        guard let baseURL = urlComponent.url else { return }
+        
+        //query items
+        let apiQuery = URLQueryItem(name: "api_key", value: Constants.giphyApiKey)
+        let ratingQuery = URLQueryItem(name: "rating", value: "pg")
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [apiQuery, ratingQuery]
+        
+        //creating a request from the URL
+        guard let urlRequest = urlComponents?.url else { completion([]) ; return }
+        print(urlRequest)
+        
+        //data task
+        URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+
+            if let error = error {
+                completion([]) ; print("\(error) : \(error.localizedDescription)") ;return
+            }
+            guard let data = data else { completion([]); return }
+
+            //Decode
+            do {
+                let decoder = JSONDecoder()
+                let topLevelDictionary = try decoder.decode(APIListResponse.self, from: data)
+                completion(topLevelDictionary.data)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
 }
--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -2,5 +2,5 @@ enum Constants {}
 
 extension Constants {
   // Get an API key from https://developers.giphy.com/dashboard/
-  static let giphyApiKey = ""
+  static let giphyApiKey = "1XyHwakiUAAS2h7kiIU3lQrIQcHk71a6"
 }
