9927bf8337bd2c375b2286eb8757743a6f80296c
Jesse Rae <51697381+AlphaDVLPR@users.noreply.github.com>
2022-01-23 19:20:43 -0700

    update

--- Dispo Take Home/Controllers/ImageFetcher.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageFetcher.swift
-//  Dispo Take Home
-//
-//  Created by Jesse Cacanindin on 1/23/22.
-//
-
-import Foundation
-
---
+++ Dispo Take Home/Views/Custom Cells/GifCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GifCollectionViewCell.swift
+//  Dispo Take Home
+//
+//  Created by Jesse Cacanindin on 1/23/22.
+//
+
+import UIKit
+
+class GifCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
